b1fb2d0f46ccbf2857e7c818ba599c73d3091982
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-03-25 06:33:37 +0800

    fix: fix POST function to generate id and create date automatically

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import pool from "./db";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/data", async (req, res) => {
   try {
@@ -26,8 +28,8 @@ app.post("/create", async (req, res) => {
       res.status(400).json({ error: "Scanned data already exists" });
     } else {
       const insertResult = await pool.query(
-        "INSERT INTO scanqr (id, created_at, scanned_data) VALUES ($1, NOW(), $2) RETURNING *",
-        [id, scannedData]
+        "INSERT INTO scanqr (created_at, scanned_data) VALUES (NOW(), $1) RETURNING *",
+        [scannedData]
       );
       res.status(201).json(insertResult.rows[0]);
     }
