4b5c40d0cef7411736f210f4bc5189daaeb4f6fc
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-03-24 23:57:48 +0800

    feat: add functions to add data and get all data

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,41 @@
-const express = require("express");
+import express from "express";
+import pool from "./db";
+import bodyParser from "body-parser";
+
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel! 🎉"));
+app.get("/data", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM scanqr");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.post("/create", async (req, res) => {
+  const { id, scannedData } = req.body;
+  try {
+    const checkResult = await pool.query(
+      "SELECT * FROM scanqr WHERE scanned_data = $1",
+      [scannedData]
+    );
 
-module.exports = app;
+    if (checkResult.rows.length > 0) {
+      res.status(400).json({ error: "Scanned data already exists" });
+    } else {
+      const insertResult = await pool.query(
+        "INSERT INTO scanqr (id, created_at, scanned_data) VALUES ($1, NOW(), $2) RETURNING *",
+        [id, scannedData]
+      );
+      res.status(201).json(insertResult.rows[0]);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
